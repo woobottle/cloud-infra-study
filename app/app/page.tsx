b2fb8c391a6eb0a02bf7081906b29a507f1e84d4
wooBottle <dkrnfls@gmail.com>
2026-04-12 20:54:24 +0900

    feat: update main page title

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>hello world it's cloud infra lab</h1>
+        <h1>hello world it's cloud infra lab 2</h1>
       </main>
     </div>
   );
